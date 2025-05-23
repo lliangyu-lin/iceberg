5914bb5f2e4231090af345fb452d37b593340aab
Leon Lin <lianglin.021@gmail.com>
2025-05-22 23:28:00 -0700

    Parsing

--- core/src/test/java/org/apache/CreateTableAction.java
+++ core/src/test/java/org/apache/CreateTableAction.java
@@ -1,34 +1,31 @@
 /*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *  * Licensed to the Apache Software Foundation (ASF) under one
- *  * or more contributor license agreements.  See the NOTICE file
- *  * distributed with this work for additional information
- *  * regarding copyright ownership.  The ASF licenses this file
- *  * to you under the Apache License, Version 2.0 (the
- *  * "License"); you may not use this file except in compliance
- *  * with the License.  You may obtain a copy of the License at
- *  *
- *  *   http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing,
- *  * software distributed under the License is distributed on an
- *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  * KIND, either express or implied.  See the License for the
- *  * specific language governing permissions and limitations
- *  * under the License.
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
-
 package org.apache;
 
 import java.util.List;
 import java.util.Map;
-import org.apache.commons.compress.utils.Lists;
 import org.apache.iceberg.Action;
 import org.apache.iceberg.ExecutionContext;
 import org.apache.iceberg.PartitionSpec;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.catalog.TableIdentifier;
+import org.apache.iceberg.relocated.com.google.common.collect.Lists;
 import org.apache.iceberg.types.Type;
 import org.apache.iceberg.types.Types;
 
@@ -50,11 +47,12 @@ public class CreateTableAction implements Action {
   public void execute(ExecutionContext context) {
     List<Types.NestedField> fields = Lists.newArrayList();
     for (int i = 0; i < schema.size(); i++) {
-      SchemaField f = schema.get(i);
-      Type parsedType = Types.fromPrimitiveString(f.type);
-      Types.NestedField nf = f.required
-              ? Types.NestedField.required(i, f.name, parsedType, f.doc)
-              : Types.NestedField.optional(i, f.name, parsedType, f.doc);
+      SchemaField schemaField = schema.get(i);
+      Type parsedType = Types.fromPrimitiveString(schemaField.type);
+      Types.NestedField nf =
+          schemaField.required
+              ? Types.NestedField.required(i, schemaField.name, parsedType, schemaField.doc)
+              : Types.NestedField.optional(i, schemaField.name, parsedType, schemaField.doc);
       fields.add(nf);
     }
 
@@ -62,6 +60,13 @@ public class CreateTableAction implements Action {
     PartitionSpec spec = PartitionSpec.unpartitioned(); // TODO: parse from JSON if needed
 
     TableIdentifier ident = TableIdentifier.parse(tableId);
-    context.catalog().createTable(ident, icebergSchema, spec, location == null || location.isEmpty() ? null : location, properties);
+    context
+        .catalog()
+        .createTable(
+            ident,
+            icebergSchema,
+            spec,
+            location == null || location.isEmpty() ? null : location,
+            properties);
   }
 }
--- data/src/integration/java/org/apache/iceberg/data/TestRunner.java
+++ data/src/integration/java/org/apache/iceberg/data/TestRunner.java
@@ -1,42 +1,39 @@
 /*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *  * Licensed to the Apache Software Foundation (ASF) under one
- *  * or more contributor license agreements.  See the NOTICE file
- *  * distributed with this work for additional information
- *  * regarding copyright ownership.  The ASF licenses this file
- *  * to you under the Apache License, Version 2.0 (the
- *  * "License"); you may not use this file except in compliance
- *  * with the License.  You may obtain a copy of the License at
- *  *
- *  *   http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing,
- *  * software distributed under the License is distributed on an
- *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  * KIND, either express or implied.  See the License for the
- *  * specific language governing permissions and limitations
- *  * under the License.
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.iceberg.data;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 import org.apache.iceberg.Action;
 import org.apache.iceberg.ExecutionContext;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class TestRunner {
 
-  private Path testSpec;
+  private static Path testSpec;
 
-  @BeforeEach
-  public void setup() {
+  @BeforeAll
+  public static void beforeAll() {
     String specPath = System.getProperty("specPath");
     if (specPath == null) {
       throw new IllegalArgumentException("Missing -DspecPath");
@@ -51,12 +48,10 @@ public class TestRunner {
     Map<String, Object> specJson = mapper.readValue(testSpec.toFile(), new TypeReference<>() {});
     List<Action> actions = mapper.convertValue(specJson.get("actions"), new TypeReference<>() {});
 
-//    ExecutionContext ctx = new ExecutionContext("/tmp/iceberg");
+    ExecutionContext ctx = new ExecutionContext("/tmp/iceberg");
 
     for (Action action : actions) {
-      System.out.println("Running action: " + action.getClass().getSimpleName());
-//      action.execute(ctx);
+      action.execute(ctx);
     }
   }
-
 }
